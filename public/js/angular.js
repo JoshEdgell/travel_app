8114c633255e594e3e80939261079f93f7f3bf9e
JoshEdgell <josh.0.edgell@gmail.com>
2018-11-27 14:23:28 -0500

    searchDirections route written, needs to be checked

--- public/js/angular.js
+++ public/js/angular.js
@@ -1,8 +1,23 @@
 const app = angular.module('WeatherApp', []);
 
 app.controller('MainController', ['$http', function($http){
+  const controller = this;
   this.word = 'poop';
   this.searchDirections = function(){
     console.log('search directions clicked');
+    $http({
+      method: 'POST',
+      url: '/directions',
+      data: {
+        origin: this.originAddress,
+        destination: this.destinationAddress
+      }
+    }).then(
+      function(response){
+        console.log(response, 'response from this.searchDirections');
+      }, function(error){
+        console.log(error, 'error from this.searchDirections');
+      }
+    )
   }
 }])
