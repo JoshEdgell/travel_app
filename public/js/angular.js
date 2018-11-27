c5970dd3cfafbfe9baab002cd6e65e184d717e77
JoshEdgell <josh.0.edgell@gmail.com>
2018-11-27 14:39:00 -0500

    basic layout created

--- public/js/angular.js
+++ public/js/angular.js
@@ -4,20 +4,23 @@ app.controller('MainController', ['$http', function($http){
   const controller = this;
   this.word = 'poop';
   this.searchDirections = function(){
-    console.log('search directions clicked');
-    $http({
-      method: 'POST',
-      url: '/directions',
-      data: {
-        origin: this.originAddress,
-        destination: this.destinationAddress
-      }
-    }).then(
-      function(response){
-        console.log(response, 'response from this.searchDirections');
-      }, function(error){
-        console.log(error, 'error from this.searchDirections');
-      }
-    )
-  }
+    // let originAddress = this.originAddress.replace(/ /g, '+')
+    // let destinationAddress = this.destinationAddress.replace(/ /g, '+')
+    let directionsObject = {
+      origin: this.originAddress.replace(/ /g, '+'),
+      destination: this.destinationAddress.replace(/ /g, '+')
+    }
+    console.log(directionsObject);
+    // $http({
+    //   method: 'POST',
+    //   url: '/directions',
+    //   data: directionsObject
+    // }).then(
+    //   function(response){
+    //     console.log(response, 'response from this.searchDirections');
+    //   }, function(error){
+    //     console.log(error, 'error from this.searchDirections');
+    //   }
+    // )
+  };
 }])
