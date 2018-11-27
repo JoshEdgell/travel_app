74d8c9a9eda57c27ccdda2c73c49ec1a34feebfe
JoshEdgell <josh.0.edgell@gmail.com>
2018-11-27 13:31:06 -0500

    installed body-parser and request

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express             = require('express');
+const bodyParser          = require('body-parser');
+const request             = require('request');
 const app                 = express();
 const router              = express.Router();
 
 // Middeware
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
   res.render(index.html);
 });
+
+const directionsController = require('./controllers/googleAPI.js');
+app.use('/directions', directionsController);
 
 const port = 3000;
 
