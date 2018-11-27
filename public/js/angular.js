174129937ba724f7e71b354db36737f24ea92188
JoshEdgell <josh.0.edgell@gmail.com>
2018-11-27 14:20:27 -0500

    directions form added to app

--- public/js/angular.js
+++ public/js/angular.js
@@ -1,5 +1,8 @@
 const app = angular.module('WeatherApp', []);
 
 app.controller('MainController', ['$http', function($http){
-  this.word = 'poop'
+  this.word = 'poop';
+  this.searchDirections = function(){
+    console.log('search directions clicked');
+  }
 }])
