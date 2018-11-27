bc522cfb5d8d675c8913cb811d912ff0db96760b
Jamie Bort <jamiebort@gmail.com>
2018-11-27 15:21:54 -0500

    Added a bunch of files to be tracked. Now commiting them.

--- controllers/googleAPI.js
+++ controllers/googleAPI.js
@@ -1,35 +1,37 @@
-const express         = require('express');
-const router          = express.Router();
+const express = require('express');
+const router = express.Router();
+
+//   What I need here is express documentation.
+
+// We're sending a post so that we can get our driving directions.
+router.post('/', (req, res) => {
+    // Receive req.body. 
+    // {
+    // origin: "103+Clearcreek+Ct,+Cary,+NC+27513",     (a string that contains spaces)
+    // destination: "807+West+Markham+Ave,+Durham,+NC+27701"     (a string that contains spaces)
+    // }
+
+    // We want to make a call to the google api.
+    request
+        // .get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&key=' + apiKey)
+        .get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&key=' + "AIzaSyDHBfRF4V8XyVyxPHXfgsQUkeIYGNp4ooI")
+
+        // We will receive a response.
+        .on('response', function (response) { // make sure we respond with json object
+            console.log(response.statusCode) // 200
+            console.log(response.headers['content-type']) // 'image/png'
+
+            // We will want to sent that back to the front end.
+        });
+        // .pipe(request.put('http://mysite.com/img.png'))
 
 
-// Get a joke
-router.get('/:id', (req,res)=>{
-    jokes.find({ _id: req.params.id }, function(error, joke){
-      res.send(joke);
-    })
-  });
-  
-  // Create new joke
-router.post('/', (req,res)=>{
-    jokes.create(req.body, (error, newJoke)=>{
-      res.json(newJoke);
-    })
-   
-  // Edit joke (have to check, also have to edit joke in User's created jokes, as well as in favorited jokes for users)
-  router.put('/:id', (req,res)=>{
-    jokes.findByIdAndUpdate(req.params.id, req.body, { new: true },
-    (err, update)=>{
-      res.json(update);
-    })
-  });
-  
-  // Delete joke
-  router.delete('/:id', (req,res)=>{
-    jokes.findByIdAndRemove(req.params.id, (error, deletedJoke)=>{
-      res.json(deletedJoke);
-    })
-  });
+    
+    
 
 
+
+
+});
 
 module.exports = router;
