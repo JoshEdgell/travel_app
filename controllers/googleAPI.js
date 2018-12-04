f312f08c0de0fee12ded50880133c06bdf68dcd9
Jamie Bort <jamiebort@gmail.com>
2018-12-04 10:35:23 -0500

    Working on accessing body.routes[0].legs[0].steps[0].polyline.points but receiving "undefined" messages.

--- controllers/googleAPI.js
+++ controllers/googleAPI.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const request = require('request');
 
 // We're sending a post so that we can get our driving directions.
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     // Receive req.body. 
     // {
     // origin: "103+Clearcreek+Ct,+Cary,+NC+27513",     (a string that contains spaces)
@@ -21,13 +21,38 @@ router.get('/', (req, res) => {
         // console.log('statusCode:', response && response.statusCode); // Prints the response status code if a response was received.
         // console.log('body:', body); // Prints the HTML for the Google homepage.
         res.json(body);
-        
-      });
+
+        // Need to get this info to the front end.
+
+        // console.log(body);
+        // console.log(body.routes);
+        // console.log(body.status);
+        // console.log(body.routes[0].legs[0].steps[0].polyline.points);
+
+        let newbody = body;
+        // console.log(newbody);
+
+        // setTimeout(function(){console.log(body.routes[0]); }, 3000);
+
+        // setTimeout(function(){console.log("hello"); }, 3000);
+
+        // console.log(newbody.routes);
+        // console.log(newbody.routes[0].legs[0].steps[0].polyline.points);
+
+        // console.log(body.keys(routes[0]));
+
+        // console.log(JSON.stringify(config))
+
+        // console.log(req.body);
+        // console.log(req.body.title);
+        // console.log(req);
+
+    });
 
 
-// NOTE - WE DO NOT NEED ANYTHING THAT IS COMMENTED OUT BELLOW.
- 
- // request
+    // NOTE - WE DO NOT NEED ANYTHING THAT IS COMMENTED OUT BELLOW.
+
+    // request
     //     // .get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&key=' + apiKey)
 
     //     // .get('https://maps.googleapis.com/maps/api/directions/json?origin=' + directionsObject.origin + '&destination=' + directionsObject.destination + '&key=' + "AIzaSyDHBfRF4V8XyVyxPHXfgsQUkeIYGNp4ooI")
@@ -36,7 +61,7 @@ router.get('/', (req, res) => {
 
     //     // We will receive a response.
     //     .on('response', function (response, error, body) { // make sure we respond with json object
-            
+
     //         console.log(response.request);
     //         res.json(response);
 
@@ -45,7 +70,7 @@ router.get('/', (req, res) => {
 
     //         // We will want to sent that back to the front end.
     //     });
-        // .pipe(request.put('http://mysite.com/img.png'))
+    // .pipe(request.put('http://mysite.com/img.png'))
 });
 
 module.exports = router;
