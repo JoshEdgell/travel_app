858ece9a0fcb6dd6007145bbc0a19e92d1f02caf
Jamie Bort <jamiebort@gmail.com>
2019-01-09 16:00:21 -0500

    Added the following scratchJamie\/SampleMapWithDotEnv\/server.js and scratchJamie\/SampleMapWithDotEnv\/public\/index.html files. Currently setting up a server with Express.

---
+++ scratchJamie/SampleMapWithDotEnv/server.js
@@ -0,0 +1,8 @@
+const express             = require('express');
+// const bodyParser          = require('body-parser'); // I don't think we need this.
+// const request             = require('request'); // I don't think we need this.
+// const dotenv              = require('dotenv').config();
+// const app                 = express();
+// const router              = express.Router();
+
+// Middeware
