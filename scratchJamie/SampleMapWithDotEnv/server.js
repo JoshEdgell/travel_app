c25306a66001e246ae8ddce44edabcce225e6ee0
Jamie Bort <jamiebort@gmail.com>
2019-01-10 13:12:37 -0500

    Upated three files and deleted one.

--- scratchJamie/SampleMapWithDotEnv/server.js
+++ scratchJamie/SampleMapWithDotEnv/server.js
@@ -1,8 +1,16 @@
-const express             = require('express');
+const express = require('express');
 // const bodyParser          = require('body-parser'); // I don't think we need this.
 // const request             = require('request'); // I don't think we need this.
-// const dotenv              = require('dotenv').config();
-// const app                 = express();
+const dotenv              = require('dotenv').config();
+const app                 = express();
 // const router              = express.Router();
 
 // Middeware
+app.use(express.static('public'));
+
+
+const port = 3000;
+
+app.get('/hello', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
